a05fc8d5a7859428f91eab9d724a37954bff0288
katsushika <marcoderio850@gmail.com>
2021-08-18 09:24:43 -0500

    graficaAlumnos

---
+++ clases/graficaAlumnos.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo Gráfica</title>
+</head>
+<body>
+    <h3>Gráfica de promedios de alumnos</h3><hr>
+    <?php
+        include "libchart/classes/libchart.php";
+
+        $chart = new HorizontalBarChart(600, 170);
+        $dataset = new XDataset();
+
+        $f = fopen("alumnos.txt", "r");
+        while (!feof($fp)) {
+            //$linea = fgets($fp);
+            fscanf($fp, "%s %f %d", $nombre, $promedio, $edad);
+            $dataset->addPoint(new Point($nombre, $promedio));
+        }
+        fclose($f);
+
+        $chart->setDataset($dataset);
+        $chart->getplot()->setGraphPadding(new Padding(5, 30, 20, 140));
+        $chart->setTitle("Promedios de los Alumnos");
+    ?>
+</body>
+</html>
