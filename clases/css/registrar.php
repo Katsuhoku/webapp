f225f14f859cb8d4141aafc65ac00d098703e805
Esiel Arizmendi <esiel.arizmendi@alumno.buap.mx>
2021-09-29 10:34:41 -0500

    CSS

---
+++ clases/css/registrar.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>videoteca</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+</head>
+<body>
+<div id="wrap">
+  <div id="masthead">
+    <h1>Videoteca</h1>
+    <div id="menucontainer">
+      <div id="menunav">
+        <ul>
+          <li><a href="index.php" ><span>Inicio</span></a></li>
+          <li><a href="peliculas.php" ><span>Peliculas</span></a></li>
+          <li><a href="registrar.php" class="current"><span>Registrar</span></a></li>
+          <li><a href="iniciar_sesion.php"><span>Iniciar sesión</span></a></li>
+          <li><a href="acerca_de_nosotros.php"><span>Acerca de nosotros</span></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div id="container">
+    <div id="content">
+      <h2>Registro </h2>
+      <p>En esta p&aacute;gina encontraras las &eacute;liculas de mayor &eacute;xito a nivel internacional. Para poder editar alguna p&eacute;lucila debes estar registrado. </p>
+      <p>&nbsp;</p>
+    </div>
+  </div>
+</div>
+<div id="footer"><br/>
+This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 License</a> </div>
+</body>
+</html>
---
+++ clases/css/validarUsuario.php
@@ -0,0 +1,26 @@
+<?php
+    $usu=$_REQUEST['usu'];
+    $pas=$_REQUEST['passwd'];
+    echo "Usuario = $usu <br>";
+    echo "Password = $pas <br>";
+
+
+    $link = mysqli_connect("localhost", "root", "");
+    mysqli_select_db($link, "videoteca");
+    $result = mysqli_query($link, "select usuario, password from clientes where usuario='$usu'");
+    
+    if ($row = mysqli_fetch_array($result)) {
+        if ($row['password'] == $pas) {
+            header("Location: indexUsuario.php");
+            //echo "Usuario registrado";
+        } else {
+            echo "Error en password";
+        } 
+    } else {
+        echo "Usuario no existe";
+    }
+
+    mysqli_free_result($result);
+    mysqli_close($link);
+
+?>
